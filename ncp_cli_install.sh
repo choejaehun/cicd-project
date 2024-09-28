fc66ce2226b31e2f2b20a58bf1ac3101282f96d2
choejaehun <balloon8866@gmail.com>
2024-09-29 01:35:19 +0900

    수정

--- ncp_cli_install.sh
+++ ncp_cli_install.sh
@@ -4,12 +4,17 @@ wget https://www.ncloud.com/api/support/download/files/cli/CLI_1.1.19_20240321.z
 unzip CLI_1.1.19_20240321
 cd CLI_1.1.19_20240321/cli_linux/
 cp ncloud /usr/bin
+echo "ncloud configure"
+
 ACCESS_KEY=$(ncloud secretmanager get-secret-value --secret-name NCP_ACCESS_KEY --query 'SecretString' --output text)
 SECRET_KEY=$(ncloud secretmanager get-secret-value --secret-name NCP_SECRET_KEY --query 'SecretString' --output text)
 export NCLOUD_ACCESS_KEY=$ACCESS_KEY
 export NCLOUD_SECRET_KEY=$SECRET_KEY
+echo "Key 적용 완료"
+
 curl -X GET "https://kr.object.ncloudstorage.com/jenkins-plugin/natgw.tf" \
      -H "X-NCP-Access-Key: $NCLOUD_ACCESS_KEY" \
      -H "X-NCP-Secret-Key: $NCLOUD_SECRET_KEY" \
      -o /root/natgw.tf
 cd
+echo "Bucket 다운로드 완료" 
