74939bd839f1e7d5c3c07af9560e89472a521c54
choejaehun <balloon8866@gmail.com>
2024-09-30 11:11:36 +0900

    mount 해제 및 privileged 추가 설정

--- ncp_cli_install.sh
+++ ncp_cli_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ehco "ncloud Configure 설치를 시작합니다."
+
 wget https://www.ncloud.com/api/support/download/files/cli/CLI_1.1.19_20240321.zip
 unzip CLI_1.1.19_20240321
 cd CLI_1.1.19_20240321/cli_linux/
@@ -10,7 +12,7 @@ ACCESS_KEY=$(ncloud secretmanager get-secret-value --secret-name NCP_ACCESS_KEY 
 SECRET_KEY=$(ncloud secretmanager get-secret-value --secret-name NCP_SECRET_KEY --query 'SecretString' --output text)
 export NCLOUD_ACCESS_KEY=$ACCESS_KEY
 export NCLOUD_SECRET_KEY=$SECRET_KEY
-echo "Key 적용 완료"
+echo "Access_Key, Secret_Key 적용 완료"
 
 curl -X GET "https://kr.object.ncloudstorage.com/jenkins-plugin/natgw.tf" \
      -H "X-NCP-Access-Key: $NCLOUD_ACCESS_KEY" \
